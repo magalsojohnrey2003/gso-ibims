dbf970629712a9f7bed3f56b547d52852058c695
magalso <magalsojohnrey2003@gmail.com>
2025-11-20 22:38:26 +0000

    Stop tracking cached view files

--- storage/framework/views/7e92a601ec713377fa619254a7fabb2a.php
+++
@@ -1,3 +0,0 @@
-<svg class="w-4 h-4 text-green-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
-</svg><?php /**PATH /home/u928333042/domains/gsoibims-tagoloan.com/public_html/storage/framework/views/e2db23d804b48c54bfff7adead136ad2.blade.php ENDPATH**/ ?>
